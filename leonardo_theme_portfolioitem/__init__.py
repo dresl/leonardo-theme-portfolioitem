918080cd0d3818e0d74e53322b017260bb1414ff
dresl <nikicresl@gmail.com>
2016-07-07 12:06:12 +0200

    style webkit scrollbar

--- leonardo_theme_portfolioitem/__init__.py
+++ leonardo_theme_portfolioitem/__init__.py
@@ -7,9 +7,11 @@ LEONARDO_APPS = [
 LEONARDO_CSS_FILES = [
     'css/animate.css',
     'css/fakeLoader.css',
+    'css/nprogress.css',
 ]
 
 LEONARDO_JS_FILES = [
     'js/fakeLoader.min.js',
     'js/portfolio.js',
+    'js/nprogress.js',
 ]
